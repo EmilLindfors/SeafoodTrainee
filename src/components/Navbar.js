ccb367d2c4b34ff91aaf3c1d34df01972cf7bfb5
Emil Lindfors <Emil.Lindfors@student.uib.no>
2018-09-09 14:03:46 +0200

    added homepage

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,8 @@
-import React from 'react'
-import Link from 'gatsby-link'
+import React from "react";
+import Link from "gatsby-link";
 
-import github from '../img/github-icon.svg'
-import logo from '../img/logo.svg'
+import github from "../img/github-icon.svg";
+import logo from "../img/logo.svg";
 
 const Navbar = () => (
   <nav className="navbar is-transparent">
@@ -10,11 +10,14 @@ const Navbar = () => (
       <div className="navbar-brand">
         <Link to="/" className="navbar-item">
           <figure className="image">
-            <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
+            <img src={logo} alt="Kaldi" style={{ width: "88px" }} />
           </figure>
         </Link>
       </div>
       <div className="navbar-start">
+        <Link className="navbar-item" to="/home">
+          Home
+        </Link>
         <Link className="navbar-item" to="/about">
           About
         </Link>
@@ -36,6 +39,6 @@ const Navbar = () => (
       </div>
     </div>
   </nav>
-)
+);
 
-export default Navbar
+export default Navbar;
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,20 +1,38 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import Helmet from 'react-helmet'
+import React from "react";
+import PropTypes from "prop-types";
+import Helmet from "react-helmet";
 
-import Navbar from '../components/Navbar'
-import './all.sass'
+import Navbar from "../components/Navbar";
+import "./all.sass";
 
 const TemplateWrapper = ({ children }) => (
   <div>
-    <Helmet title="Home | Gatsby + Netlify CMS" />
+    <Helmet>
+      <title>Seafood Trainee</title>
+      <script
+        type="text/javascript"
+        src="https://identity.netlify.com/v1/netlify-identity-widget.js"
+      />
+      <script type="text/javascript">
+        {`
+  if (window.netlifyIdentity) {
+    window.netlifyIdentity.on("init", user => {
+      if (!user) {
+        window.netlifyIdentity.on("login", () => {
+          document.location.href = "/admin/";
+        });
+      }
+    });
+  }`}
+      </script>
+    </Helmet>
     <Navbar />
     <div>{children()}</div>
   </div>
-)
+);
 
 TemplateWrapper.propTypes = {
-  children: PropTypes.func,
-}
+  children: PropTypes.func
+};
 
-export default TemplateWrapper
+export default TemplateWrapper;
